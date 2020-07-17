a419973c97df2cf2719daa36f84d5a50ab47b44e
Britannia Lomax <Britannia.Lomax@premex.com>
2020-07-17 11:58:20 +0100

    adding DnaPairs

---
+++ Katas/Core/DnaPairs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Katas.Core
+{
+    public class DnaPairs
+    {
+        public List<char[]> pairsList;
+        private readonly Dictionary<char, char> pairMapping = new Dictionary<char, char>()
+        {
+            { 'G', 'C' },
+            { 'C', 'G' },
+            { 'T', 'A' },
+            { 'A', 'T' }
+        };
+
+        public DnaPairs(string dna)
+        {
+            pairsList = new List<char[]>();
+            if (dna != String.Empty)
+            {
+                if (dna == "G")
+                {
+                    pairsList.Add(new char[] { 'G', pairMapping['G'] });
+                }
+            }
+        }
+
+    }
+}
---
+++ Testing/DnaPairsTests.cs
@@ -0,0 +1,37 @@
+﻿using Katas.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing
+{
+    public class DnaPairsTests
+    {
+        [Fact]
+        public void DnaPairs_Valid_CheckReturnsEmptyWhenPassedEmptyString()
+        {
+            // arrange
+            string empty = String.Empty;
+
+            // act
+            var dnaPairs = new DnaPairs(empty);
+
+            // assert
+            Assert.Empty(dnaPairs.pairsList);
+        }
+
+        [Fact]
+        public void DnaPairs_Valid_CheckReturnsPairValueWhenPassedOneDNAValue()
+        {
+            // arrange
+            string dnaString = "G";
+
+            // act
+            var dnaPairs = new DnaPairs(dnaString);
+
+            // assert
+            Assert.Equal(dnaPairs.pairsList, new List<char[]>() { new char[] { 'G', 'C' } });
+        }
+    }
+}
