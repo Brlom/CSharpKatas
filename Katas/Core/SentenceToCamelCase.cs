76aeadbfe5518894e613c363ce692400ada85fb8
brlom <brlom007@gmail.com>
2020-07-18 22:26:46 +0100

    adding ConvertToCamelCase

---
+++ Katas/Core/SentenceToCamelCase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Katas.Core
+{
+    public class SentenceToCamelCase
+    {
+        public string ConvertToCamelCase(string strToConvert, bool upperOrLower)
+        {
+            return strToConvert;
+        }
+    }
+}
---
+++ Testing/SentenceToCamelCaseTests.cs
@@ -0,0 +1,25 @@
+﻿using Katas.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing
+{
+    public class SentenceToCamelCaseTests
+    {
+        [Fact]
+        public void SentenceToCamelCase_WhenPassedEmptyStr_ShouldReturnEmptyStr()
+        {
+            // arrange
+            string empty = String.Empty;
+            SentenceToCamelCase x = new SentenceToCamelCase();
+
+            // act
+            string actual = x.ConvertToCamelCase("", false);
+
+            // assert
+            Assert.Equal(empty, actual);
+        }
+    }
+}
