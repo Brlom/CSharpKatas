fc185fd1df1c0476d6c3845223c13b03eb74aa05
brlom <brlom007@gmail.com>
2020-07-19 17:05:35 +0100

    adding solution for SentenceToCamelCase/ConvertToEnglish

--- Katas/Core/SentenceToCamelCase.cs
+++ Katas/Core/SentenceToCamelCase.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Xunit.Sdk;
 
 namespace Katas.Core
 {
@@ -8,7 +12,74 @@ namespace Katas.Core
     {
         public string ConvertToCamelCase(string strToConvert, bool upperOrLower)
         {
-            return strToConvert;
+            if (strToConvert == "")
+            {
+                return strToConvert;
+            }
+
+            TextInfo ti = new CultureInfo("en-GB", false).TextInfo;
+            string[] wordArr = strToConvert.Split(" ");
+            string newStr = String.Empty;
+
+            if (upperOrLower)
+            {
+                foreach (string word in wordArr)
+                {
+                    newStr += ti.ToTitleCase(word);
+                }
+            }
+            else
+            {
+                newStr += wordArr[0];
+                foreach (string word in wordArr)
+                {
+                    if (word != wordArr[0])
+                    {
+                        newStr += ti.ToTitleCase(word);
+                    }
+                }
+            }
+            return newStr;
+        }
+
+        public string ConvertToEnglish(string strToConvert)
+        {
+            if(strToConvert == "")
+            {
+                return strToConvert;
+            }
+
+            char[] charArr = strToConvert.ToCharArray();
+            string newStr = String.Empty;
+            Regex uppercase = new Regex(@"[A-Z]");
+
+            foreach(Char ch in charArr)
+            {
+                if (uppercase.IsMatch(ch.ToString()) && ch != charArr[0])
+                {
+                    newStr += " " + ch;
+                }
+                else
+                {
+                    newStr += ch;
+                }
+            }
+
+            string[] wordArr = newStr.Split(" ");
+            string returningStr = String.Empty;
+
+            TextInfo ti = new CultureInfo("en-GB", false).TextInfo;
+            returningStr += ti.ToTitleCase(wordArr[0]);
+
+            foreach (string word in wordArr)
+            {
+                if (word != wordArr[0])
+                {
+                    returningStr += " " + word.ToLower();
+                }
+            }
+
+            return returningStr;
         }
     }
 }
--- Testing/SentenceToCamelCaseTests.cs
+++ Testing/SentenceToCamelCaseTests.cs
@@ -1,4 +1,5 @@
 ﻿using Katas.Core;
+using NuGet.Frameworks;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -21,5 +22,97 @@ namespace Testing
             // assert
             Assert.Equal(empty, actual);
         }
+
+        [Fact]
+        public void SentenceToCamelCase_WhenPassedStrAndTrue_ShouldReturnUpperCamelCaseStr()
+        {
+            // arrange
+            string original = "this sentence";
+            bool shouldItBeUpperCamelCase = true;
+            SentenceToCamelCase x = new SentenceToCamelCase();
+            string expectedReturn = "ThisSentence";
+
+            // act
+            string actual = x.ConvertToCamelCase(original, shouldItBeUpperCamelCase);
+
+            // assert
+            Assert.Equal(expectedReturn, actual);
+        }
+
+        [Fact]
+        public void SentenceToCamelCase_WhenPassedStrAndFalse_ShouldReturnLowerCamelCaseStr()
+        {
+            // arrange
+            string original = "this sentence";
+            bool shouldItBeUpperCamelCase = false;
+            SentenceToCamelCase x = new SentenceToCamelCase();
+            string expectedReturn = "thisSentence";
+
+            // act
+            string actual = x.ConvertToCamelCase(original, shouldItBeUpperCamelCase);
+
+            // assert
+            Assert.Equal(expectedReturn, actual);
+        }
+
+        [Fact]
+        public void SentenceToCamelCase_WhenPassedStrangeStrAndTrue_ShouldReturnUpperCamelCaseStr()
+        {
+            // arrange
+            string original = "This Bigger strange Sentence";
+            bool shouldItBeUpperCamelCase = true;
+            SentenceToCamelCase x = new SentenceToCamelCase();
+            string expectedReturn = "ThisBiggerStrangeSentence";
+
+            // act
+            string actual = x.ConvertToCamelCase(original, shouldItBeUpperCamelCase);
+
+            // assert
+            Assert.Equal(expectedReturn, actual);
+        }
+
+        [Fact]
+        public void SentenceToEnglish_WhenPassedEmptyStr_ShouldReturnEmptyStr()
+        {
+            // arrange
+            string empty = String.Empty;
+            SentenceToCamelCase x = new SentenceToCamelCase();
+
+            // act 
+            string actual = x.ConvertToEnglish(empty);
+
+            // assert 
+            Assert.Equal(empty, actual);
+        }
+
+        [Fact]
+        public void SentenceToEnglish_WhenPassedCamelCaseStr_ShouldReturnNormalStr()
+        {
+            // arrange
+            string original = "ThisBiggerStrangeSentence";
+            SentenceToCamelCase x = new SentenceToCamelCase();
+            string expectedReturn = "This bigger strange sentence";
+
+            // act
+            string actual = x.ConvertToEnglish(original);
+
+            // assert
+            Assert.Equal(expectedReturn, actual);
+        }
+        
+        [Fact]
+        public void SentenceToEnglish_WhenPassedLowerCamelCaseStr_ShouldReturnNormalStr()
+        {
+            // arrange
+            string original = "thisBiggerStrangeSentence";
+            SentenceToCamelCase x = new SentenceToCamelCase();
+            string expectedReturn = "This bigger strange sentence";
+
+            // act
+            string actual = x.ConvertToEnglish(original);
+
+            // assert
+            Assert.Equal(expectedReturn, actual);
+        }        
     }
 }
