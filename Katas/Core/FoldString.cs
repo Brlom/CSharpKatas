4cc39524a2b598c0a7ede9fffe8fa1c40cb5de69
brlom <brlom007@gmail.com>
2020-07-19 17:14:16 +0100

    adding setup for FoldString kata

---
+++ Katas/Core/FoldString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Katas.Core
+{
+    public class FoldString
+    {
+        public string StringFolder(string str)
+        {
+            return str;
+        }
+
+    }
+}
---
+++ Testing/FoldStringTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing
+{
+    public class FoldStringTests
+    {
+        [Fact]
+        public void FoldString_WhenPassedEmptyStr_ShouldReturnEmptyStr()
+        { 
+        
+        }
+
+    }
+}
