d329c2745742e5cb5a6eb1f4ebb2d2a55f61b069
brlom <brlom007@gmail.com>
2020-07-21 22:12:33 +0100

    started on FoldString - still working on punctuation test

--- Katas/Core/FoldString.cs
+++ Katas/Core/FoldString.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Katas.Core
 {
@@ -8,7 +10,58 @@ namespace Katas.Core
     {
         public string StringFolder(string str)
         {
-            return str;
+            if (str == String.Empty)
+            {
+                return str;
+            }
+
+            Regex punctuation = new Regex(@"[.,!?]");
+            if (punctuation.IsMatch(str))
+            { 
+            
+            }
+
+            string newStr = String.Empty;
+            string[] splitStr = str.Split(" ");
+
+            foreach (string word in splitStr)
+            { 
+                if (word.Length % 2 == 0)
+                {
+                    string firstHalf = word.Substring(0, word.Length / 2);
+                    string secondHalf = word.Substring(word.Length / 2);
+
+                    char[] firstArr = firstHalf.ToCharArray();
+                    char[] secondArr = secondHalf.ToCharArray();
+
+                    Array.Reverse(firstArr);
+                    Array.Reverse(secondArr);
+
+                    string backStrFirst = new string(firstArr);
+                    string backStrSecond = new string(secondArr);
+
+                    newStr += backStrFirst + backStrSecond + " ";
+                }   
+                else 
+                {
+                    string oddFirstHalf = word.Substring(0, word.Length / 2);
+                    string oddSecondHalf = word.Substring(word.Length / 2 + 1);
+                    string middle = word.Substring(word.Length/2).Substring(0, 1);
+
+                    char[] oddFirstArr = oddFirstHalf.ToCharArray();
+                    char[] oddSecondArr = oddSecondHalf.ToCharArray();
+
+                    Array.Reverse(oddFirstArr);
+                    Array.Reverse(oddSecondArr);
+
+                    string convertToStringOddFirst = new string(oddFirstArr);
+                    string convertToStringOddSecond = new string(oddSecondArr);
+
+                    newStr += convertToStringOddFirst + middle + convertToStringOddSecond + " ";
+                }
+            }
+
+            return newStr.Trim(' ');
         }
 
     }
--- Testing/FoldStringTests.cs
+++ Testing/FoldStringTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Katas.Core;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -9,9 +10,84 @@ namespace Testing
     {
         [Fact]
         public void FoldString_WhenPassedEmptyStr_ShouldReturnEmptyStr()
-        { 
-        
+        {
+            // arrange
+            string empty = String.Empty;
+            FoldString x = new FoldString();
+
+            // act
+            string actual = x.StringFolder(empty);
+
+            // assert
+            Assert.Equal(empty, actual);
         }
 
+        [Fact]
+        public void FoldString_WhenPassedEvenStr_ShouldReturnFoldedStr()
+        {
+            // arrange
+            string original = "code";
+            string original2 = "abcdef";
+            string original3 = "javascript";
+            FoldString x = new FoldString();
+            string expected = "oced";
+            string expected2 = "cbafed";
+            string expected3 = "savajtpirc";
+
+            // act
+            string actual = x.StringFolder(original);
+            string actual2 = x.StringFolder(original2);
+            string actual3 = x.StringFolder(original3);
+
+            // assert
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected2, actual2);
+            Assert.Equal(expected3, actual3);
+        }
+
+        [Fact]
+        public void FoldString_WhenPassedOddStr_ShouldReturnFoldeStr()
+        {
+            // arrange
+            string original = "Northcoders";
+            FoldString x = new FoldString();
+            string expected = "htroNcsredo";
+
+            // act
+            string actual = x.StringFolder(original);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FoldString_WhenPassedStrWithSpaces_ShouldReturnFoldedStrWithSpaces()
+        {
+            // arrange
+            string original = "javascript is cool";
+            FoldString x = new FoldString();
+            string expected = "savajtpirc is oclo";
+
+            // act
+            string actual = x.StringFolder(original);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        //[Fact]
+        //public void FoldString_WhenPassedStrWithPunctuation_ShouldReturnFoldedStrWithPunct()
+        //{
+        //    // arrange
+        //    string original = "javascript is cool.";
+        //    FoldString x = new FoldString();
+        //    string expected = "savajtpirc is oclo.";
+
+        //    // act
+        //    string actual = x.StringFolder(original);
+
+        //    // assert
+        //    Assert.Equal(expected, actual);
+        //}
     }
 }
