9c15ee2352698098735124a0e2052d216c37c5f3
brlom <brlom007@gmail.com>
2020-07-24 18:38:44 +0100

    adding HerdTheBabies

---
+++ Katas/Core/HerdTheBabies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Katas.Core
+{
+    public class HerdTheBabies
+    {
+
+        public string SortTheBabies(string original)
+        {
+            return original;
+        }
+
+    }
+}
---
+++ Testing/HerdTheBabiesTests.cs
@@ -0,0 +1,26 @@
+﻿using Katas.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing
+{
+    public class HerdTheBabiesTests
+    {
+        [Fact]
+        public void SortTheBabies_WhenPassedAnEmptyStr_ShouldReturnEmptyStr()
+        {
+            // arrange
+            string original = String.Empty;
+            HerdTheBabies x = new HerdTheBabies();
+
+            // act
+            string actual = x.SortTheBabies(original);
+
+            // assert
+            Assert.Equal(original, actual);
+        }
+
+    }
+}
