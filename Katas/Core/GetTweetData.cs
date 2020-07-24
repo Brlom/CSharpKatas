d9606368650f095e470c8a5003763d3107665397
brlom <brlom007@gmail.com>
2020-07-24 18:29:06 +0100

    adding core tests/functionality for GetTweetData

---
+++ Katas/Core/GetTweetData.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Katas.Core
+{
+    public class GetTweetData
+    {
+        public TweetDataObject ReturnTweetData(string originalStr)
+        {
+            if (originalStr == String.Empty)
+            {
+                return null;
+            }
+
+            TweetDataObject newDataObject = new TweetDataObject
+            {
+                Tags = FindTags(originalStr),
+                Mentions = FindMentions(originalStr),
+                TagCount = FindTags(originalStr).Count,
+                MentionCount = FindMentions(originalStr).Count,
+                StringLength = originalStr.Length
+            };
+
+            return newDataObject;
+        }
+
+        public List<string> FindMentions(string fullStr)
+        {
+            if (fullStr == String.Empty)
+            {
+                return new List<string>();
+            }
+
+            var mentionsMatch = Regex.Matches(fullStr, @"[@][A-Za-z]+")
+                .OfType<Match>()
+                .Select(m => m.Value)
+                .ToList<string>();
+
+            return mentionsMatch.Distinct().ToList<string>();
+        }
+
+        public List<string> FindTags(string fullStr)
+        {
+            if (fullStr == String.Empty)
+            {
+                return new List<string>();
+            }
+
+            var tagsMatch = Regex.Matches(fullStr, @"[#][A-Za-z]+")
+                .OfType<Match>()
+                .Select(m => m.Value)
+                .ToList<string>();
+
+            return tagsMatch.Distinct().ToList<string>();
+        }
+    }
+
+    public class TweetDataObject
+    {
+        public List<string> Tags { get; set; }
+        public List<string> Mentions { get; set; }
+        public int TagCount { get; set; }
+        public int MentionCount { get; set; }
+        public int StringLength { get; set; }
+    }
+
+}
---
+++ Testing/GetTweetDataTests.cs
@@ -0,0 +1,173 @@
+﻿using Katas.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing
+{
+    public class GetTweetDataTests
+    {
+        [Fact]
+        public void GetTweetData_WhenPassedEmptyStr_ShouldReturnEmptyObject()
+        {
+            // arrange
+            string empty = String.Empty;
+            GetTweetData x = new GetTweetData();
+            Object expected = null;
+
+            // act
+            Object actual = x.ReturnTweetData(empty);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetTweetData_WhenPassedStrWithNoTweets_ShouldReturnTweetDataObjectWithStrLength()
+        {
+            // arrange
+            string original = "My awesome tweet";
+            GetTweetData x = new GetTweetData();
+            var tagList = new List<string>();
+            var mentionsList = new List<string>();
+            TweetDataObject expected = new TweetDataObject
+            {
+                Tags = tagList,
+                Mentions = mentionsList,
+                TagCount = 0,
+                MentionCount = 0,
+                StringLength = 16
+            };
+
+            // act
+            TweetDataObject actual = x.ReturnTweetData(original);
+
+            // assert
+            Assert.Equal(expected.Tags, actual.Tags);
+            Assert.Equal(expected.Mentions, actual.Mentions);
+            Assert.Equal(expected.TagCount, actual.TagCount);
+            Assert.Equal(expected.MentionCount, actual.MentionCount);
+            Assert.Equal(expected.StringLength, actual.StringLength);
+        }
+
+        [Fact]
+        public void GetTweetData_WhenPassedStrWithAMention_ShouldReturnTweetDataObjectWithMentionAndNumMentions()
+        {
+            // arrange
+            string original = "My awesome tweet to @northcoders";
+            GetTweetData x = new GetTweetData();
+            var tagList = new List<string>();
+            var mentionsList = new List<string>();
+            mentionsList.Add("@northcoders");
+            TweetDataObject expected = new TweetDataObject
+            {
+                Tags = tagList,
+                Mentions = mentionsList,
+                TagCount = 0,
+                MentionCount = 1,
+                StringLength = 32
+            };
+
+            // act
+            TweetDataObject actual = x.ReturnTweetData(original);
+
+            // assert
+            Assert.Equal(expected.Tags, actual.Tags);
+            Assert.Equal(expected.Mentions, actual.Mentions);
+            Assert.Equal(expected.TagCount, actual.TagCount);
+            Assert.Equal(expected.MentionCount, actual.MentionCount);
+            Assert.Equal(expected.StringLength, actual.StringLength);
+        }
+
+        [Fact]
+        public void GetTweetData_WhenPassedStrWithATag_ShouldReturnTweetDataObjectWithTagAndNumTags()
+        {
+            // arrange
+            string original = "My awesome tweet about #coding";
+            GetTweetData x = new GetTweetData();
+            var tagList = new List<string>();
+            tagList.Add("#coding");
+            var mentionsList = new List<string>();
+            TweetDataObject expected = new TweetDataObject
+            {
+                Tags = tagList,
+                Mentions = mentionsList,
+                TagCount = 1,
+                MentionCount = 0,
+                StringLength = 30
+            };
+
+            // act 
+            TweetDataObject actual = x.ReturnTweetData(original);
+
+            // assert
+            Assert.Equal(expected.Tags, actual.Tags);
+            Assert.Equal(expected.Mentions, actual.Mentions);
+            Assert.Equal(expected.TagCount, actual.TagCount);
+            Assert.Equal(expected.MentionCount, actual.MentionCount);
+            Assert.Equal(expected.StringLength, actual.StringLength);
+        }
+
+        [Fact]
+        public void GetTweetData_WhenPassedStrWithATagAndAMention_ShouldReturnTweetDataObjWithAllValues()
+        {
+            // arrange
+            string original = "My awesome tweet about #coding to @northcoders";
+            GetTweetData x = new GetTweetData();
+            var tagList = new List<string>();
+            tagList.Add("#coding");
+            var mentionsList = new List<string>();
+            mentionsList.Add("@northcoders");
+            TweetDataObject expected = new TweetDataObject
+            {
+                Tags = tagList,
+                Mentions = mentionsList,
+                TagCount = 1,
+                MentionCount = 1,
+                StringLength = 46
+            };
+
+            // act
+            TweetDataObject actual = x.ReturnTweetData(original);
+
+            // assert
+            Assert.Equal(expected.Tags, actual.Tags);
+            Assert.Equal(expected.Mentions, actual.Mentions);
+            Assert.Equal(expected.TagCount, actual.TagCount);
+            Assert.Equal(expected.MentionCount, actual.MentionCount);
+            Assert.Equal(expected.StringLength, actual.StringLength);
+        }
+
+        [Fact]
+        public void GetTweetData_WhenPassedStrWithSeveralTagsAndMentions_ShouldReturnUpdatedTweetDataObject()
+        {
+            // arrange
+            string original = "I am #coding with @northcoders I love #coding and @northcoders";
+            GetTweetData x = new GetTweetData();
+            var tagList = new List<string>();
+            tagList.Add("#coding");
+            var mentionsList = new List<string>();
+            mentionsList.Add("@northcoders");
+            TweetDataObject expected = new TweetDataObject
+            {
+                Tags = tagList,
+                Mentions = mentionsList,
+                TagCount = 1,
+                MentionCount = 1,
+                StringLength = 62
+            };
+
+            // act
+            TweetDataObject actual = x.ReturnTweetData(original);
+
+            // assert
+            Assert.Equal(expected.Tags, actual.Tags);
+            Assert.Equal(expected.Mentions, actual.Mentions);
+            Assert.Equal(expected.TagCount, actual.TagCount);
+            Assert.Equal(expected.MentionCount, actual.MentionCount);
+            Assert.Equal(expected.StringLength, actual.StringLength);
+        }
+
+    }
+}
