18b3354240290925630fa244fc0b7c08338a48d6
Britannia Lomax <Britannia.Lomax@premex.com>
2020-08-11 16:04:02 +0100

    adding initial testing/code for RotateArray

--- Katas/Core/RotateArray.cs
+++ Katas/Core/RotateArray.cs
@@ -6,10 +6,59 @@ namespace Katas.Core
 {
     public class RotateArray
     {
-        public List<int> RotateListElements(List<int> intList, int numRotatations)
+        public List<int> RotateListElements(List<int> intList, int numRotations)
         {
-            return intList;
-        }
+            var itemsToKeep = new List<int>();
+            var itemsToRotate = new List<int>();
 
+            var finalList = new List<int>();
+            int actualRotations = (Math.Abs(numRotations) > intList.Count) ? Math.Abs(numRotations) % intList.Count : Math.Abs(numRotations);
+            int removeFirst = intList.Count - actualRotations;
+
+            foreach (int i in intList)
+            {
+                itemsToKeep.Add(i);
+                itemsToRotate.Add(i);
+            }
+
+            if (numRotations >= 0 && intList.Count < 2)
+            {
+                return intList;
+            }
+
+            if (numRotations < 0)
+            {
+                itemsToRotate.RemoveRange(actualRotations, removeFirst);
+                itemsToKeep.RemoveRange(0, actualRotations);
+
+                foreach (int i in itemsToKeep)
+                {
+                    finalList.Add(i);
+                }
+
+                foreach (int i in itemsToRotate)
+                {
+                    finalList.Add(i);
+                }
+
+            }
+            else
+            {
+                itemsToKeep.RemoveRange(removeFirst, actualRotations);
+                itemsToRotate.RemoveRange(0, removeFirst);
+
+                foreach (int i in itemsToRotate)
+                {
+                    finalList.Add(i);
+                }
+
+                foreach (int i in itemsToKeep)
+                {
+                    finalList.Add(i);
+                }
+            }
+
+            return finalList;
+        }
     }
 }
--- Testing/RotateArrayTests.cs
+++ Testing/RotateArrayTests.cs
@@ -12,6 +12,7 @@ namespace Testing
             // arrange
             var empty = new List<int>();
             int numRotations = 0;
+
             var x = new RotateArray();
 
             // act
@@ -21,5 +22,114 @@ namespace Testing
             Assert.Equal(empty, actual);
         }
 
+        [Fact]
+        public void RotateListElements_ShouldReturnUnchangedList_WhenPassedListAndZeroRotations()
+        {
+            // arrange
+            var original = new List<int>() { 1, 2, 3 };
+            int numRotations = 0;
+            var expected = original;
+
+            var x = new RotateArray();
+
+            // act
+            var actual = x.RotateListElements(original, numRotations);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void RotateListElements_ShouldReturnListRotatedOnce_WhenPassedListAndASingleRotation()
+        {
+            // arrange
+            var original = new List<int>() { 1, 2, 3 };
+            int numRotations = 1;
+            var expected = new List<int>() { 3, 1, 2 };
+
+            var original5 = new List<int>() { 1, 2, 3, 4, 5 };
+            int numRotations5 = 3;
+            var expected5 = new List<int> { 3, 4, 5, 1, 2 };
+
+            var x = new RotateArray();
+            
+            // act 
+            var actual = x.RotateListElements(original, numRotations);
+            var actual5 = x.RotateListElements(original5, numRotations5);
+
+            // assert
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected5, actual5);
+        }
+
+        [Fact]
+        public void RotateListElements_ShouldReturnListRotatedToLeft_WhenPassedNegativeRotationNum()
+        {
+            // arrange
+            var original = new List<int>() { 1, 2, 3 };
+            int numRotations = -1;
+            var expected = new List<int>() { 2, 3, 1 };
+
+            var original5 = new List<int>() { 1, 2, 3, 4, 5 };
+            int numRotations5 = -3;
+            var expected5 = new List<int>() { 4, 5, 1, 2, 3 };
+
+            var x = new RotateArray();
+            
+            // act
+            var actual = x.RotateListElements(original, numRotations);
+            var actual5 = x.RotateListElements(original5, numRotations5);
+
+            // assert
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected5, actual5);
+        }
+
+        [Fact]
+        public void RotateListElements_ShouldRotateList_WhenPassedNumHigherThanCount()
+        {
+            // arrange
+            var original = new List<int> { 1, 2, 3 };
+            int numRotations = 5;
+            var expected = new List<int> { 2, 3, 1 };
+            
+            var original5 = new List<int> { 1, 2, 3, 4, 5 };
+            int numRotations5 = 13;
+            var expected5 = new List<int> { 3, 4, 5, 1, 2 };
+            
+            var x = new RotateArray();
+            
+            // act 
+            var actual = x.RotateListElements(original, numRotations);
+            var actual5 = x.RotateListElements(original5, numRotations5);
+
+            // assert
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected5, actual5);
+        }
+
+        [Fact]
+        public void RotateListElements_WhenPassedIntListAndNegNumLargerThanListCount_ShouldReturnListRotated()
+        {
+            // arrange
+            var original = new List<int>() { 1, 2, 3 };
+            int numRotations = -4;
+            var expected = new List<int>() { 2, 3, 1 };
+
+            var original6 = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            int numRotations6 = -16;
+            var expected6 = new List<int>() { 5, 6, 1, 2, 3, 4 };
+
+            var x = new RotateArray();
+
+            // act
+            var actual = x.RotateListElements(original, numRotations);
+            var actual6 = x.RotateListElements(original6, numRotations6);
+
+            // assert
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected, actual);
+
+        }
     }
 }
