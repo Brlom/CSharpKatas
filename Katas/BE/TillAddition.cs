12583f1995c06961251773cf9fa65d91da23d7a3
Britannia Lomax <Britannia.Lomax@premex.com>
2020-08-11 17:27:34 +0100

    changing structure to accomodate different types of katas, starting on tests/code for TillAddition.cs

---
+++ Katas/BE/TillAddition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Katas.BE
+{
+    public class TillAddition
+    {
+        public string GetSumOfCashInTill(Dictionary<string, int> tillContents)
+        {
+            if (tillContents.Count == 0)
+            { 
+                return "£0.00";
+            }
+
+            double totalSum = 0;
+
+            foreach(KeyValuePair<string, int> entry in tillContents)
+            {
+                // entry.Value && entry.Key
+                if (entry.Value != 0)
+                {
+                    char[] charArr = entry.Key.ToCharArray();
+                    foreach (char c in charArr)
+                    {
+                        double numericCharValue = Char.GetNumericValue(c);
+
+                        if (numericCharValue.GetType() == typeof(int))
+                        {
+                            totalSum += numericCharValue * entry.Value;
+                        }
+                    }
+                    // convert entry.key to int
+                }
+            }
+            return "£0.00";
+        }
+    }
+}
---
+++ Testing/BETests/TillAdditionTests.cs
@@ -0,0 +1,152 @@
+﻿using Katas.BE;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Testing.BETests
+{
+    public class TillAdditionTests
+    {
+        [Fact]
+        public void GetSumOfCashInTill_WhenPassedEmptyDict_ShouldReturnZero()
+        {
+            // arrange
+            var original = new Dictionary<string, int>();
+            string expected = "£0.00";
+
+            var x = new TillAddition();
+
+            // act
+            var actual = x.GetSumOfCashInTill(original);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetSumOfCashInTill_WhenPassedDict_ShouldReturnSum()
+        {
+            // arrange
+            var original = new Dictionary<string, int>()
+            {
+                { "1p", 1 },
+                { "2p", 1 }
+            };
+            string expected = "£0.03";
+
+            var x = new TillAddition();
+
+            // act
+            var actual = x.GetSumOfCashInTill(original);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        //[Fact]
+        //public void GetSumOfCashInTill_WhenPassedDictWithFiveCoins_ShouldReturnSum()
+        //{
+        //    // arrange
+        //    var original = new Dictionary<string, int>()
+        //    {
+        //        { "1p", 1 },
+        //        { "2p", 1 },
+        //        { "5p", 1 },
+        //        { "10p", 1 },
+        //        { "20p", 1 }
+        //    };
+        //    string expected = "£0.38";
+
+        //    var x = new TillAddition();
+
+        //    // act
+        //    var actual = x.GetSumOfCashInTill(original);
+
+        //    // assert
+        //    Assert.Equal(expected, actual);
+        //}
+
+        //[Fact]
+        //public void GetSumOfCashInTill_WhenPassedDictWithSixCoins_ShouldReturnSum()
+        //{
+        //    // arrange
+        //    var original = new Dictionary<string, int>()
+        //    {
+        //        { "1p", 1 },
+        //        { "2p", 1 },
+        //        { "5p", 1 },
+        //        { "10p", 1 },
+        //        { "20p", 1 },
+        //        { "50p", 1 }, 
+        //        { "£1", 1 }
+        //    };
+        //    string expected = "£1.85";
+
+        //    var x = new TillAddition();
+
+        //    // act
+        //    var actual = x.GetSumOfCashInTill(original);
+
+        //    // assert
+        //    Assert.Equal(expected, actual);
+        //}
+        //[Fact]
+        //public void GetSumOfCashInTill_WhenPassedDictWithCoinAndBills_ShouldReturnSum()
+        //{
+        //    // arrange
+        //    var original = new Dictionary<string, int>()
+        //    {
+        //        { "1p", 1 },
+        //        { "2p", 1 },
+        //        { "5p", 1 },
+        //        { "10p", 1 },
+        //        { "20p", 1 },
+        //        { "50p", 1 },
+        //        { "£1", 1 },
+        //        { "£2", 1 },
+        //        { "£5", 1 },
+        //        { "£10", 1 },
+        //        { "£20", 1 },
+        //        { "£50", 1 }
+        //    };
+        //    string expected = "£80.85";
+
+        //    var x = new TillAddition();
+
+        //    // act
+        //    var actual = x.GetSumOfCashInTill(original);
+
+        //    // assert
+        //    Assert.Equal(expected, actual);
+        //}
+
+        //[Fact]
+        //public void GetSumOfCashInTill_WhenPassedSeveralOfEachEntity_ShouldReturnSum()
+        //{
+        //    // arrange
+        //    var original = new Dictionary<string, int>()
+        //    {
+        //        { "1p", 10 },
+        //        { "2p", 3 },
+        //        { "5p", 1 },
+        //        { "10p", 6 },
+        //        { "20p", 1 },
+        //        { "50p", 1 },
+        //        { "£1", 5 },
+        //        { "£2", 1 },
+        //        { "£5", 1 },
+        //        { "£10", 1 },
+        //        { "£20", 1 },
+        //        { "£50", 4 }
+        //    };
+        //    string expected = "£243.51";
+
+        //    var x = new TillAddition();
+
+        //    // act
+        //    var actual = x.GetSumOfCashInTill(original);
+
+        //    // assert
+        //    Assert.Equal(expected, actual);
+        //}
+    }
+}
