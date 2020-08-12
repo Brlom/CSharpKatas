9e2cce1de27f3c12aa21de9e9511757426c768ec
Britannia Lomax <Britannia.Lomax@premex.com>
2020-08-12 18:40:27 +0100

    adding initial testing/code for ChangeCalculator.cs

---
+++ Katas/BE/ChangeCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Katas.BE
+{
+    public class ChangeCalculator
+    {
+        public Dictionary<string, double> CalculateChange(double sumChangeDue)
+        {
+            var finalDict = new Dictionary<string, double>();
+            var coins = new List<double>() { 50, 20, 10, 5, 2, 1 };
+            double remainingChangeDue = sumChangeDue;
+            
+            foreach (double coin in coins)
+            { 
+                if(remainingChangeDue > 0)
+                { 
+                    double numCoins = 0;
+                    if (remainingChangeDue - coin >= 0)
+                    {
+                        if (remainingChangeDue % coin == 0)
+                        {
+                            numCoins += remainingChangeDue / coin;
+                            remainingChangeDue -= coin * numCoins;
+                            finalDict.Add(coin.ToString(), numCoins);
+                        }
+                        else
+                        { 
+                            numCoins = (remainingChangeDue - (remainingChangeDue % coin)) / coin;
+                            remainingChangeDue = remainingChangeDue%coin;
+                            finalDict.Add(coin.ToString(), numCoins);
+                        }
+                    }
+                }
+            }
+
+            return finalDict;
+        }
+    }
+}
---
+++ Testing/BETests/ChangeCalculatorTests.cs
@@ -0,0 +1,117 @@
+﻿using Katas.BE;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing.BETests
+{
+    public class ChangeCalculatorTests
+    {
+        [Fact]
+        public void CalculateChange_WhenPassedNoChange_ShouldReturnEmptyObject()
+        {
+            // arrange
+            double original = 0;
+            var expected = new Dictionary<string, double>();
+
+            var x = new ChangeCalculator();
+
+            // act
+            var actual = x.CalculateChange(original);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CalculateChange_WhenPassedValueDivisibleBySingleCoinValue_ShouldReturnObjectWithOneKeyValPair()
+        {
+            // arrange
+            double original = 1;
+            var expected = new Dictionary<string, double>()
+            {
+                { "1", 1 }
+            };
+
+            double original2 = 2;
+            var expected2 = new Dictionary<string, double>()
+            {
+                { "2", 1 }
+            };
+
+            var x = new ChangeCalculator();
+
+            // act
+            var actual = x.CalculateChange(original);
+            var actual2 = x.CalculateChange(original2);
+
+            // assert
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected2, actual2);
+        }
+
+        [Fact]
+        public void CalculateChange_WhenPassedValueNotEqualToAnyKey_ShouldDivideIntoSeveralKeys_AndReturnObject()
+        {
+            // arrange
+            double original = 7;
+            var expected = new Dictionary<string, double>()
+            {
+                { "2", 1 },
+                { "5", 1 }
+            };
+
+            var x = new ChangeCalculator();
+
+            // act
+            var actual = x.CalculateChange(original);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CalculateChange_WhenPassedDoubleDigitNum_ShouldStillReturnCorrectObject()
+        {
+            //arrange
+            double original = 13;
+            var expected = new Dictionary<string, double>()
+            {
+                { "1", 1 },
+                { "2", 1 },
+                { "10", 1 }
+            };
+
+            var x = new ChangeCalculator();
+
+            // act
+            var actual = x.CalculateChange(original);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CalculateChange_WhenPassedTripleDigitNum_ShouldReturnObjectWithMultipleCoinsWherePossible()
+        {
+            //arrange
+            double original = 229;
+            var expected = new Dictionary<string, double>()
+            {
+                { "2", 2 },
+                { "5", 1 },
+                { "20", 1 },
+                { "50", 4 }
+            };
+
+            var x = new ChangeCalculator();
+
+            // act
+            var actual = x.CalculateChange(original);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
