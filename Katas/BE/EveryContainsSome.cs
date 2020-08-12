7202aeb771c1976d546976f11349d8d5e1a0a4b4
Britannia Lomax <Britannia.Lomax@premex.com>
2020-08-12 18:44:59 +0100

    adding EveryContainsSome.cs and test file for same

---
+++ Katas/BE/EveryContainsSome.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Katas.BE
+{
+    public class EveryContainsSome
+    {
+        public string Every()
+        {
+            return "";
+        }
+    }
+}
---
+++ Testing/BETests/EveryContainsSomeTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing.BETests
+{
+    public class EveryContainsSomeTests
+    {
+        [Fact]
+        public void EveryContainsSome_ShouldDoSomething()
+        {
+            // arrange
+
+            // act
+
+            // assert
+
+        }
+    }
+}
