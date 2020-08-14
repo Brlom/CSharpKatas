c4e193e7271c0ec6b622e27c0b89e0ec56a7dfdb
Britannia Lomax <Britannia.Lomax@premex.com>
2020-08-14 16:17:21 +0100

    completed initial tests/code, need to add functionality for multiple types

--- Katas/BE/EveryContainsSome.cs
+++ Katas/BE/EveryContainsSome.cs
@@ -4,11 +4,113 @@ using System.Text;
 
 namespace Katas.BE
 {
+    public delegate bool EvaluateInt(int x);
+
     public class EveryContainsSome
     {
-        public string Every()
+        public bool Every(List<int> intList, EvaluateInt evaluate)
         {
-            return "";
+            bool result = true;
+            
+            foreach (int item in intList)
+            { 
+                bool outcome = Evaluate(item);
+                if (outcome == false)
+                {
+                    result = false;
+                    break;
+                }
+            }
+            
+            return result;
+        }
+
+        public bool Contains(List<int> intList, int num)
+        {
+            bool result = false;
+
+            foreach (int item in intList)
+            {
+                if (item == num)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        public bool Some(List<Object> listToEvaluate)
+        {
+            bool result = false;
+           
+            foreach (Object item in listToEvaluate)
+            {
+                if (item is int && (int)item > 0)
+                {
+                    result = true;
+                    break;
+                }
+                else if (item is double && (double)item > 0)
+                {
+                    result = true;
+                    break;
+                }
+                else if (item is decimal && (decimal)item > 0)
+                {
+                    result = true;
+                    break;
+                }
+                else if (item is float && (float)item > 0)
+                {
+                    result = true;
+                    break;
+                }
+                else if (item is long && (long)item > 0)
+                {
+                    result = true;
+                    break;
+                }
+                    else if (item is short && (short)item > 0)
+                {
+                    result = true;
+                    break;
+                }
+                else if (item is bool && (bool)item == true)
+                {
+                    result = true;
+                    break;
+                }
+                else if (item is string)
+                {
+                    result = true;
+                    break;
+                }
+                else if (item is char)
+                {
+                    result = true;
+                    break;
+                }
+                else if (item is DateTime)
+                {
+                    result = true;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        public bool Evaluate(int num)
+        {
+            var result = false;
+
+            if (num > 0)
+            {
+                result = num % 2 == 0;
+            }
+
+            return result;
         }
     }
 }
--- Testing/BETests/EveryContainsSomeTests.cs
+++ Testing/BETests/EveryContainsSomeTests.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using Katas.BE;
+using System;
 using System.Collections.Generic;
-using System.Text;
 using Xunit;
 
 namespace Testing.BETests
@@ -8,14 +8,134 @@ namespace Testing.BETests
     public class EveryContainsSomeTests
     {
         [Fact]
-        public void EveryContainsSome_ShouldDoSomething()
+        public void EveryContainsSome_WhenPassedListOfNumsAndDel_ShouldReturnBool()
         {
             // arrange
+            var originalList = new List<int>()
+            {
+                2, 
+                4,
+                5
+            };
+            bool expected = false;
+
+            var originalList2 = new List<int>()
+            {
+                2,
+                4,
+                12
+            };
+            bool expected2 = true;
+
+            var x = new EveryContainsSome();
 
             // act
+            // Instantiate the delegate
+            EvaluateInt handler = x.Evaluate;
+            var actual = x.Every(originalList, handler);
+            var actual2 = x.Every(originalList2, handler);
 
             // assert
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected2, actual2);
+        }
 
+        // TODO: should make this work for other types as well.
+        [Fact]
+        public void EveryContainsSome_ShouldReturnBool_WhenPassedListAndItemInList()
+        {
+            // arrange
+            var originalList = new List<int>()
+            {
+                1,
+                2,
+                3
+            };
+            int num = 3;
+            bool expected = true;
+
+            var originalList2 = new List<int>()
+            {
+                1,
+                2,
+                3
+            };
+            int num2 = 4;
+            bool expected2 = false;
+
+            var x = new EveryContainsSome();
+
+            // act
+            var actual = x.Contains(originalList, num);
+            var actual2 = x.Contains(originalList2, num2);
+
+            // assert
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected2, actual2);
+        }
+
+        [Fact]
+        public void EveryContainsSome_ShouldReturnBool_WhenPassedListContainingTruthyVal()
+        {
+            // arrange
+            var originalList = new List<Object>()
+            {
+                null, 
+                0, 
+                "yes",
+                false
+            };
+            bool expected = true;
+
+            var originalList2 = new List<Object>()
+            {
+                null,
+                0,
+                -3.16,
+                false
+            };
+            bool expected2 = false;
+
+            var x = new EveryContainsSome();
+
+            // act
+            var actual = x.Some(originalList);
+            var actual2 = x.Some(originalList2);
+
+            // assert
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected2, actual2);
+        }
+
+        [Fact]
+        public void EveryContainsSome_Evaluate_ShouldCheckTruthinessOfPassedParamMod2_AndReturnBool()
+        {
+            // arrange
+            var original = 4;
+            var expected = true;
+
+            var original2 = 3;
+            var expected2 = false;
+
+            var original3 = 0;
+            var expected3 = false;
+
+            var original4 = -4;
+            var expected4 = false;
+
+            var x = new EveryContainsSome();
+
+            // act
+            var actual = x.Evaluate(original);
+            var actual2 = x.Evaluate(original2);
+            var actual3 = x.Evaluate(original3);
+            var actual4 = x.Evaluate(original4);
+
+            // assert
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected2, actual2);
+            Assert.Equal(expected3, actual3);
+            Assert.Equal(expected4, actual4);
         }
     }
 }
