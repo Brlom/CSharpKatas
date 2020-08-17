c23bc4968bba43e73450f3bf2bf2f55bd215be61
Britannia Lomax <Britannia.Lomax@premex.com>
2020-08-17 12:30:24 +0100

    started on tests/solution for SecondsToTimeString.cs

---
+++ Katas/BE/SecondsToTimeString.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Katas.BE
+{
+    public class SecondsToTimeString
+    {
+        public string ConvertSecondsToString(double seconds)
+        {
+            double remainingSeconds = seconds;
+            string finalStr = String.Empty;
+
+            string resultHourStr = String.Empty;
+            string resultMinuteStr = String.Empty;
+            string resultSecondStr = "seconds";
+            double resultHours = 0;
+            double resultMinutes = 0;
+            double resultSeconds = 0;
+
+            while (remainingSeconds > 0)
+            {
+                if (remainingSeconds > 60)
+                {
+                    double secondsLeftAfterMinDeducted = remainingSeconds % 60;
+                    resultMinutes = (remainingSeconds-secondsLeftAfterMinDeducted) / 60;
+                    if (resultMinutes == 1)
+                    {
+                        resultMinuteStr = "minute";
+                    }
+                    else
+                    { 
+                        resultMinuteStr = "minutes";
+                    }
+                    remainingSeconds -= resultMinutes * 60;
+                }
+                else if (remainingSeconds == 60)
+                {
+                    resultMinutes = remainingSeconds / 60;
+                    resultMinuteStr = "minute";
+                    remainingSeconds -= remainingSeconds;
+                }
+                else if (remainingSeconds > 1 && remainingSeconds < 60)
+                {
+                    resultSeconds = remainingSeconds;
+                    resultSecondStr = "seconds";
+                    remainingSeconds -= remainingSeconds;
+                }
+                else if (remainingSeconds == 1)
+                {
+                    resultSeconds = remainingSeconds;
+                    resultSecondStr = "second";
+                    remainingSeconds -= 1;
+                }
+            }
+
+            if (resultHours > 0)
+            {
+                finalStr += $"{resultHours} {resultHourStr}";
+            }
+            if (resultMinutes > 0)
+            {
+                if (resultHourStr != String.Empty)
+                {
+                    finalStr += $" {resultMinutes} {resultMinuteStr}";
+                }
+                else
+                { 
+                    finalStr += $"{resultMinutes} {resultMinuteStr}";
+                }
+            }
+            if (resultSeconds >= 0)
+            {
+                if (resultMinuteStr != String.Empty)
+                {
+                    if (resultSeconds != 0)
+                    {
+                        finalStr += $" {resultSeconds} {resultSecondStr}";
+                    }
+                }
+                else
+                {
+                    finalStr += $"{resultSeconds} {resultSecondStr}";
+                }    
+            }
+
+            return finalStr;
+        }
+    }
+}
---
+++ Testing/BETests/SecondsToTimeStringTests.cs
@@ -0,0 +1,64 @@
+﻿using Katas.BE;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing.BETests
+{
+    public class SecondsToTimeStringTests
+    {
+        [Fact]
+        public void SecondsToTimeString_ConvertSecondsToString_WhenPassedSeconds_ShouldReturnSecondsAsStr()
+        {
+            // arrange
+            double original = 0;
+            string expected = $"{original} seconds";
+
+            double original2 = 1;
+            string expected2 = $"{original2} second";
+
+            double original3 = 15;
+            string expected3 = $"{original3} seconds";
+
+            var x = new SecondsToTimeString();
+
+            // act
+            var actual = x.ConvertSecondsToString(original);
+            var actual2 = x.ConvertSecondsToString(original2);
+            var actual3 = x.ConvertSecondsToString(original3);
+
+            // assert
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected2, actual2);
+            Assert.Equal(expected3, actual3);
+        }
+
+        [Fact]
+        public void SecondsToTimeString_ConvertSecondsToString_WhenPassedSecondsAbove59_ShouldReturnCorrectlyConvertedStr()
+        {
+            // arrange
+            double original = 60;
+            string expected = "1 minute";
+
+            double original2 = 61;
+            string expected2 = "1 minute 1 second";
+
+            double original3 = 120;
+            string expected3 = "2 minutes";
+
+            var x = new SecondsToTimeString();
+
+            // act
+            string actual = x.ConvertSecondsToString(original);
+            string actual2 = x.ConvertSecondsToString(original2);
+            string actual3 = x.ConvertSecondsToString(original3);
+
+            // assert
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected2, actual2);
+            Assert.Equal(expected3, actual3);
+
+        }
+    }
+}
