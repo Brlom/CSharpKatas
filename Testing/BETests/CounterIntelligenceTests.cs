e235cac42f5f2442aed1926c801fda757758fc0e
Britannia Lomax <Britannia.Lomax@premex.com>
2020-09-17 17:17:27 +0100

    adding CounterIntelligence class, and starting on tests/solution

---
+++ Testing/BETests/CounterIntelligenceTests.cs
@@ -0,0 +1,56 @@
+﻿using Katas.BE;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing.BETests
+{
+    public class CounterIntelligenceTests
+    {
+        [Fact]
+        public void CounterIntelligence_StringDecoder_ShouldReturnStr()
+        {
+            // arrange
+            var original = String.Empty;
+            var expected = String.Empty;
+            var x = new CounterIntelligence();
+
+            // act
+            var actual = x.StringDecoder(original);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CounterIntelligence_StringDecoder_WhenPassedOneChar_ShouldReturnX()
+        {
+            // arrange
+            string original = "Y";
+            string expected = "X";
+            var x = new CounterIntelligence();
+
+            // act
+            var actual = x.StringDecoder(original);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CounterIntelligence_StringDecoder_WhenPassedStrWithNoSpace_ShouldReturnDecodedStr()
+        {
+            // arranage
+            string original = "BCDY";
+            string expected = "ABCX";
+            var x = new CounterIntelligence();
+
+            // act
+            var actual = x.StringDecoder(original);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
