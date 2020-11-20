fd0edcdc59e805c46b2bb94540151a31efe7fb85
brlom <brlom007@gmail.com>
2020-11-20 08:50:25 +0000

    adding beginnings of CounterSpy

---
+++ Katas/BE/CounterSpy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Katas.BE
+{
+    public class CounterSpy
+    {
+        public List<string> RemoveSpies(List<string> providedList)
+        {
+            return providedList;
+        }
+    }
+}
---
+++ Testing/BETests/CounterSpyTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing.BETests
+{
+    public class CounterSpyTests
+    {
+        [Fact]
+        public void RemoveSpies_ShouldReturnList()
+        { 
+            // arrange
+
+            // act
+
+            // assert
+
+        }
+    }
+}
