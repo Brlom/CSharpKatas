1378540e5235aeef83ba16288292564485b68fed
Britannia Lomax <Britannia.Lomax@premex.com>
2020-11-30 17:08:22 +0000

    making a start on ValidateAndFormat

---
+++ Katas/BE/ValidateAndFormatPhone.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Katas.BE
+{
+    public class ValidateAndFormatPhone
+    {
+        public string SendToTwilio(string enteredVal)
+        {
+            if (!CheckIfValid(enteredVal))
+            {
+                return "Invalid phone number";
+            }
+            return enteredVal;
+        }
+
+        public bool CheckIfValid(string number)
+        {
+            if (string.IsNullOrEmpty(number))
+            {
+                return false;
+            }
+            else if (number.All(char.IsDigit))
+            {
+
+                return true;
+            }
+            return false;
+        }
+
+        public string FormatPhoneNum(string validNum)
+        {
+            return "";
+        }
+    }
+}
---
+++ Testing/BETests/ValidateAndFormatPhoneTests.cs
@@ -0,0 +1,30 @@
+﻿using Katas.BE;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing.BETests
+{
+    public class ValidateAndFormatPhoneTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("08559912983")]
+        [InlineData("This is not a valid number")]
+        public void SendToTwilio_ShouldReturnInvalid_WhenValidationFails(string enteredNum)
+        {
+            // arrange
+            string expected = "Invalid phone number";
+
+            var sut = new ValidateAndFormatPhone();
+
+            // act
+            string actual = sut.SendToTwilio(enteredNum);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
