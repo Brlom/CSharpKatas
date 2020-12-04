7f24e2eebc1066478599d26e5d15eec123ad28fa
Britannia Lomax <Britannia.Lomax@premex.com>
2020-12-04 10:44:53 +0000

    adding additional testing/code for ValidateAndFormatPhone kata

--- Katas/BE/ValidateAndFormatPhone.cs
+++ Katas/BE/ValidateAndFormatPhone.cs
@@ -13,7 +13,11 @@ namespace Katas.BE
             {
                 return "Invalid phone number";
             }
-            return enteredVal;
+            else
+            {
+                string formattedNum = FormatPhoneNum(enteredVal);
+                return formattedNum;
+            }
         }
 
         public bool CheckIfValid(string number)
@@ -22,17 +26,72 @@ namespace Katas.BE
             {
                 return false;
             }
-            else if (number.All(char.IsDigit))
+            else if (number.All(char.IsDigit) && number.Length == 11 && number.StartsWith("0"))
             {
-
-                return true;
+                string chopZeroString = number.Substring(1);
+                if (chopZeroString.StartsWith("1") || chopZeroString.StartsWith("2") || chopZeroString.StartsWith("7"))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (number.All(char.IsDigit) && number.Length == 14 && number.StartsWith("0"))
+            {
+                string chopFirstTwo = number.Substring(2);
+                if (chopFirstTwo.StartsWith("4"))
+                {
+                    string chopNextThree = chopFirstTwo.Substring(2);
+                    if (chopNextThree.StartsWith("1") || chopNextThree.StartsWith("2") || chopNextThree.StartsWith("7"))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            else if (number.StartsWith("+") && number.Length == 13)
+            {
+                string chopFirstChar = number.Substring(1);
+                string chopFirstThreeChars = number.Substring(3);
+                if (chopFirstChar.StartsWith("4") && (chopFirstThreeChars.StartsWith("1") || chopFirstThreeChars.StartsWith("2") || chopFirstThreeChars.StartsWith("7")))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
             return false;
         }
 
         public string FormatPhoneNum(string validNum)
         {
-            return "";
+            string chopFirstChar = validNum.Substring(1);
+            string chopFirstThreeChars = validNum.Substring(3);
+
+            if (validNum.All(char.IsDigit) && validNum.Length == 11 && validNum.StartsWith("0"))
+            {
+                return $"+44{chopFirstChar}";
+            }
+            else if (validNum.All(char.IsDigit) && validNum.Length == 14 && validNum.StartsWith("0"))
+            {
+                string chopZeroString = validNum.Substring(2);
+                return $"+{chopZeroString}";
+            }
+            else if (validNum.Length == 13 && chopFirstChar.StartsWith("4") && (chopFirstThreeChars.StartsWith("1") || chopFirstThreeChars.StartsWith("2") || chopFirstThreeChars.StartsWith("7")))
+            {
+                return validNum;
+            }
+            else
+            {
+                return String.Empty;
+            }
         }
     }
 }
--- Testing/BETests/ValidateAndFormatPhoneTests.cs
+++ Testing/BETests/ValidateAndFormatPhoneTests.cs
@@ -12,6 +12,7 @@ namespace Testing.BETests
         [InlineData(null)]
         [InlineData("")]
         [InlineData("08559912983")]
+        [InlineData("+2082394891246")]
         [InlineData("This is not a valid number")]
         public void SendToTwilio_ShouldReturnInvalid_WhenValidationFails(string enteredNum)
         {
@@ -22,6 +23,70 @@ namespace Testing.BETests
 
             // act
             string actual = sut.SendToTwilio(enteredNum);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SendToTwilio_ShouldReturnFormattedNum_WhenValidPrivateNumProvided()
+        {
+            // arrange
+            string original = "01616603202";
+            string expected = "+441616603202";
+
+            var sut = new ValidateAndFormatPhone();
+
+            // act
+            string actual = sut.SendToTwilio(original);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SendToTwilio_ShouldReturnFormattedNum_WhenLongerValidNumProvided()
+        {
+            // arrange
+            string original = "00441616603202";
+            string expected = "+441616603202";
+
+            var sut = new ValidateAndFormatPhone();
+
+            // act
+            var actual = sut.SendToTwilio(original);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SendToTwilio_ShouldReturnFormattedNum_WhenValid0DigitNumProvided()
+        {
+            // arrange
+            string original = "07540706000";
+            string expected = "+447540706000";
+
+            var sut = new ValidateAndFormatPhone();
+
+            // act
+            var actual = sut.SendToTwilio(original);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SendToTwilio_ShouldReturnFormattedNum_WhenValid44DigitNumProvided()
+        {
+            // arrange
+            string original = "+447540706000";
+            string expected = "+447540706000";
+
+            var sut = new ValidateAndFormatPhone();
+
+            // act
+            var actual = sut.SendToTwilio(original);
 
             // assert
             Assert.Equal(expected, actual);
