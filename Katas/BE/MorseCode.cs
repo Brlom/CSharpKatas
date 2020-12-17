1109db88b7a824ce696243021368ddc455781c87
Britannia Lomax <Britannia.Lomax@premex.com>
2020-12-17 15:51:43 +0000

    starting on MorseCode

---
+++ Katas/BE/MorseCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Katas.BE
+{
+    public class MorseCode
+    {
+        public string MorseDecode(string morseCode)
+        {
+            return String.Empty;
+        }
+    }
+}
---
+++ Testing/BETests/MorseCodeTests.cs
@@ -0,0 +1,28 @@
+﻿using Katas.BE;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing.BETests
+{
+    public class MorseCodeTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void DecodeMorse_ShouldReturnEmptyString_WhenPassedNullOrEmpty(string morseCode)
+        {
+            // arrange
+            string expected = string.Empty;
+
+            var sut = new MorseCode();
+
+            // act
+            var actual = sut.MorseDecode(morseCode);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
