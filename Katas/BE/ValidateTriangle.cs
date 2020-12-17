4970f32e9048b66afafeb692d64e67282258447f
Britannia Lomax <Britannia.Lomax@premex.com>
2020-12-17 15:32:11 +0000

    adding initial code and tests for ValidTriangles.cs

---
+++ Katas/BE/ValidateTriangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Katas.BE
+{
+    public class ValidateTriangle
+    {
+        public int FindValidTriangles(List<List<int>> triangleList)
+        {
+            int numTriangles = 0;
+
+            foreach (List<int> triangle in triangleList)
+            {
+                if (triangle.Count == 0)
+                {
+                    numTriangles += 0;
+                }
+                else
+                {
+                    int firstSum = triangle[0] + triangle[1];
+                    int secondSum = triangle[1] + triangle[2];
+                    int thirdSum = triangle[2] + triangle[0];
+
+                    if (firstSum > triangle[2] && secondSum > triangle[0] && thirdSum > triangle[1])
+                    {
+                        numTriangles++;
+                    }
+                }
+            }
+
+            return numTriangles;
+        }
+    }
+}
---
+++ Testing/BETests/ValidateTriangleTests.cs
@@ -0,0 +1,121 @@
+﻿using Katas.BE;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing.BETests
+{
+    public class ValidateTriangleTests
+    {
+        [Fact]
+        public void FindValidTriangles_ShouldReturnZero_WhenPassedEmptyList()
+        {
+            // arrange
+            var empty = new List<int>();
+            var triangles = new List<List<int>>();
+            triangles.Add(empty);
+
+            int expected = 0;
+
+            var sut = new ValidateTriangle();
+
+            // act
+            var actual = sut.FindValidTriangles(triangles);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FindValidTriangles_ShouldReturnZero_WhenPassedListContainingNoTriangles()
+        {
+            // arrange
+            int[] fakeTriangle = { 5, 10, 25 };
+            var noTriangles = new List<int>(fakeTriangle);
+
+            var triangles = new List<List<int>>();
+            triangles.Add(noTriangles);
+
+            int expected = 0;
+
+            var sut = new ValidateTriangle();
+
+            // act
+            var actual = sut.FindValidTriangles(triangles);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FindValidTriangles_ShouldReturnNumTriangles_WhenPassedListContainingValidTriangle()
+        {
+            // arrange
+            int[] validTriangle = { 5, 4, 5 };
+            var triangle = new List<int>(validTriangle);
+
+            var triangles = new List<List<int>>();
+            triangles.Add(triangle);
+
+            int expected = 1;
+
+            var sut = new ValidateTriangle();
+
+            // act
+            var actual = sut.FindValidTriangles(triangles);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FindValidTriangles_ShouldReturnNumValidTrianglesInList()
+        {
+            // arrange
+            int[] validTriangle = { 542, 586, 419 };
+            var triangle = new List<int>(validTriangle);
+
+            int[] invalidTriangle = { 829, 272, 126 };
+            var triangle2 = new List<int>(invalidTriangle);
+
+            int[] validTriangle2 = { 660, 514, 367 };
+            var triangle3 = new List<int>(validTriangle2);
+
+            int[] invalidTriangle2 = { 39, 703, 839 };
+            var triangle4 = new List<int>(invalidTriangle2);
+
+            int[] invalidTriangle3 = { 229, 861, 2 };
+            var triangle5 = new List<int>(invalidTriangle3);
+
+            int[] validTriangle3 = { 235, 952, 840 };
+            var triangle6 = new List<int>(validTriangle3);
+
+            int[] invalidTriangle4 = { 892, 533, 108 };
+            var triangle7 = new List<int>(invalidTriangle4);
+
+            int[] invalidTriangle5 = { };
+            var triangle8 = new List<int>(invalidTriangle5);
+
+            var triangles = new List<List<int>>();
+            triangles.Add(triangle);
+            triangles.Add(triangle2);
+            triangles.Add(triangle3);
+            triangles.Add(triangle4);
+            triangles.Add(triangle5);
+            triangles.Add(triangle6);
+            triangles.Add(triangle7);
+            triangles.Add(triangle8);
+
+            int expected = 3;
+
+            var sut = new ValidateTriangle();
+
+            // act
+            var actual = sut.FindValidTriangles(triangles);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
