7fb993bd27c61c0a3ccdb635ff672bb9bd44ab31
Britannia Lomax <Britannia.Lomax@premex.com>
2020-12-18 17:22:22 +0000

    adding more code/tests for MorseCode.cs

--- Katas/BE/MorseCode.cs
+++ Katas/BE/MorseCode.cs
@@ -6,9 +6,122 @@ namespace Katas.BE
 {
     public class MorseCode
     {
-        public string MorseDecode(string morseCode)
+        public string MorseDecode(string morseCode, bool toMorse)
         {
-            return String.Empty;
+            string newMsg = String.Empty;
+
+            if (!toMorse && !string.IsNullOrEmpty(morseCode))
+            {
+                var morseList = morseCode.Split(" ");
+                foreach(string morse in morseList)
+                { 
+                    newMsg += findDecodedValue(morse);
+                }
+            }
+            else if (toMorse && !string.IsNullOrEmpty(morseCode))
+            {
+                var morseStr = morseCode.ToUpper();
+                foreach (char morse in morseStr)
+                {
+                    newMsg += $"{findMorseValue(morse)} ";
+                }
+            }
+            else
+            {
+                return String.Empty;
+            }
+
+            return toMorse ? newMsg.TrimEnd(' ') : $"{newMsg.Substring(0, 1)}{newMsg.Substring(1).ToLower()}";
+        }
+
+        public string findDecodedValue(string morseCode)
+        {
+            var morseDict = new Dictionary<string, string>
+            {
+                { ".-", "A" },
+                { "-...", "B" },
+                { "-.-.", "C" },
+                { "-..", "D" },
+                { ".", "E" },
+                { "..-.", "F" },
+                { "--.", "G" },
+                { "....", "H" },
+                { "..", "I" },
+                { ".---", "J" },
+                { "-.-", "K" },
+                { ".-..", "L" },
+                { "--", "M" },
+                { "-.", "N" },
+                { "---", "O" },
+                { ".--.", "P" },
+                { "--.-", "Q" },
+                { ".-.", "R" },
+                { "...", "S" },
+                { "-", "T" },
+                { "..-", "U" },
+                { "...-", "V" },
+                { ".--", "W" },
+                { "-..-", "X" },
+                { "-.--", "Y" },
+                { "--..", "Z" },
+                { "-----", "0" },
+                { ".----", "1" },
+                { "..---", "2" },
+                { "...--", "3" },
+                { "....-", "4" },
+                { ".....", "5" },
+                { "-....", "6" },
+                { "--...", "7" },
+                { "---..", "8" },
+                { "----.", "9" }
+            };
+
+            return morseDict[morseCode];
+        }
+
+        public string findMorseValue(char morseCode)
+        {
+            var toMorseDict = new Dictionary<char, string>
+            {
+                { 'A', ".-" },
+                { 'B', "-..." },
+                { 'C', "-.-." },
+                { 'D', "-.." },
+                { 'E', "."},
+                { 'F', "..-." },
+                { 'G', "--." },
+                { 'H', "...." },
+                { 'I', ".." },
+                { 'J', ".---" },
+                { 'K', "-.-" },
+                { 'L', ".-.." },
+                { 'M', "--" },
+                { 'N', "-." },
+                { 'O', "---" },
+                { 'P', ".--." },
+                { 'Q', "--.-" },
+                { 'R', ".-." },
+                { 'S', "..." },
+                { 'T', "-" },
+                { 'U', "..-" },
+                { 'V', "...-" },
+                { 'W', ".--" },
+                { 'X', "-..-" },
+                { 'Y', "-.--" },
+                { 'Z', "--.." },
+                { '0', "-----" },
+                { '1', ".----" },
+                { '2', "..---" },
+                { '3', "...--" },
+                { '4', "....-" },
+                { '5', "....." },
+                { '6', "-...." },
+                { '7', "--..." },
+                { '8', "---.." },
+                { '9', "----." }
+            };
+
+            return toMorseDict[morseCode];
         }
     }
 }
--- Testing/BETests/MorseCodeTests.cs
+++ Testing/BETests/MorseCodeTests.cs
@@ -1,7 +1,4 @@
 ﻿using Katas.BE;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Xunit;
 
 namespace Testing.BETests
@@ -9,9 +6,11 @@ namespace Testing.BETests
     public class MorseCodeTests
     {
         [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        public void DecodeMorse_ShouldReturnEmptyString_WhenPassedNullOrEmpty(string morseCode)
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData(null, true)]
+        [InlineData("", true)]
+        public void DecodeMorse_ShouldReturnEmptyString_WhenPassedNullOrEmpty(string morseCode, bool toMorse)
         {
             // arrange
             string expected = string.Empty;
@@ -19,7 +18,56 @@ namespace Testing.BETests
             var sut = new MorseCode();
 
             // act
-            var actual = sut.MorseDecode(morseCode);
+            var actual = sut.MorseDecode(morseCode, toMorse);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("....", false, "H")]
+        [InlineData("H", true, "....")]
+        public void DecodeMorse_ShouldReturnDecodedValue_WhenPassedSingleValue(string original, bool toMorse, string expected)
+        {
+            // arrange
+            var sut = new MorseCode();
+
+            // act
+            var actual = sut.MorseDecode(original, toMorse);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+      
+        [Theory]
+        [InlineData(".... ..", false, "Hi")]
+        [InlineData("Hi", true, ".... ..")]
+        [InlineData(".... . .-.. .-.. ---", false, "Hello")]
+        [InlineData("Hello", true, ".... . .-.. .-.. ---")]
+        [InlineData("-. --- .-. - .... -.-. --- -.. . .-. ...", false, "Northcoders")]
+        [InlineData("Northcoders", true, "-. --- .-. - .... -.-. --- -.. . .-. ...")]
+        public void DecodeMorse_ShouldReturnDecodedWord_WhenPassedSingleWord(string original, bool toMorse, string expected)
+        {
+            // arrange
+            var sut = new MorseCode();
+
+            // act
+            var actual = sut.MorseDecode(original, toMorse);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("--. --- --- -..   -- --- .-. -. .. -. --.   -. --- .-. - .... -.-. --- -.. . .-. ...", false, "Good morning northcoders")]
+        [InlineData("Good morning northcoders", true, "--. --- --- -..   -- --- .-. -. .. -. --.   -. --- .-. - .... -.-. --- -.. . .-. ...")]
+        public void DecodeMorse_ShouldReturnDecodedString_WhenPassedMultipleWordStr(string original, bool toMorse, string expected)
+        {
+            // arrange
+            var sut = new MorseCode();
+
+            // act
+            var actual = sut.MorseDecode(original, toMorse);
 
             // assert
             Assert.Equal(expected, actual);
