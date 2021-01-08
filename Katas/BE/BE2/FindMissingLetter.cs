4ead415af6e08fb97863b75575fdf081ac11435a
Britannia Lomax <Britannia.Lomax@premex.com>
2021-01-08 14:51:56 +0000

    adding initial code/test cases for FindMissingLetter

--- Katas/BE/BE2/FindMissingLetter.cs
+++ Katas/BE/BE2/FindMissingLetter.cs
@@ -8,7 +8,43 @@ namespace Katas.BE.BE2
     {
         public string FindTheMissingLetter(List<char> charList)
         {
+            int ascii = 0;
+            bool isUpper = false;
+
+            foreach (char listItem in charList)
+            {
+                int charAsAscii = (int)listItem;
+
+                if (ascii == 0)
+                {
+                    ascii = (int)Char.ToLower(listItem);
+                    isUpper = CheckIfUpper(charAsAscii);
+                }
+                else if (ascii != 0 && (int)Char.ToLower(listItem) == ascii + 1)
+                {
+                    ascii = (int)Char.ToLower(listItem);
+                }
+                else
+                {
+                    var missingAscii = Convert.ToChar(ascii + 1);
+                    var upperMissing = Char.ToUpper(missingAscii);
+                    return isUpper ? Char.ToString(upperMissing) : Char.ToString(missingAscii);
+                }
+            }
+
             return string.Empty;
+        }
+
+        public bool CheckIfUpper(int listItemVal)
+        {
+            if (listItemVal <= 90)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- Testing/BETests/BE2/FindMissingLetterTests.cs
+++ Testing/BETests/BE2/FindMissingLetterTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Katas.BE.BE2;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -7,15 +8,32 @@ namespace Testing.BETests.BE2
 {
     public class FindMissingLetterTests
     {
-        [Fact]
-        public void FindTheMissingLetters_ShouldReturnAString()
+        public static IEnumerable<object[]> testData()
+        {
+            yield return new object[] { new List<char>() { 'A', 'B' }, String.Empty };
+            yield return new object[] { new List<char>() { 'a', 'b' }, String.Empty };
+            yield return new object[] { new List<char>() { 'A', 'C' }, "B" };
+            yield return new object[] { new List<char>() { 'a', 'c' }, "b" };
+            yield return new object[] { new List<char>() { 'A', 'B', 'C', 'D', 'E', 'F' }, String.Empty };
+            yield return new object[] { new List<char>() { 'a', 'b', 'c', 'd', 'e', 'f' }, String.Empty };
+            yield return new object[] { new List<char>() { 'A', 'B', 'C', 'E', 'F' }, "D" };
+            yield return new object[] { new List<char>() { 'a', 'b', 'c', 'e', 'f' }, "d"};
+            yield return new object[] { new List<char>() { 'E', 'F', 'G', 'I' }, "H" };
+            yield return new object[] { new List<char>() { 'e', 'f', 'g', 'i' }, "h" };
+        }
+
+        [Theory]
+        [MemberData(nameof(testData))]
+        public void FindTheMissingLetters_ShouldReturnAString_ContainingMissingLetter(List<char> original, string expected)
         {
             // arrange
+            var sut = new FindMissingLetter();
 
             // act 
+            var actual = sut.FindTheMissingLetter(original);
 
             // assert
-            
+            Assert.Equal(expected, actual);
         }
     }
 }
