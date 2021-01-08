fe5c5692346d8fada5bb16db47c79dabd7822c69
Britannia Lomax <Britannia.Lomax@premex.com>
2021-01-08 15:04:48 +0000

    adding class and tests for InsertNewValue

---
+++ Katas/BE/BE2/InsertNewValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Katas.BE.BE2
+{
+    public class InsertNewValue
+    {
+        public List<int> AddNewValueToList(List<int> intList, int intToAdd)
+        {
+            return new List<int>();
+        }
+    }
+}
---
+++ Testing/BETests/BE2/InsertNewValueTests.cs
@@ -0,0 +1,30 @@
+﻿using Katas.BE.BE2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing.BETests.BE2
+{
+    public class InsertNewValueTests
+    {
+        [Fact]
+        public void AddNewValueToList_ShouldReturnNewList_WithAddedValue()
+        {
+            // arrange
+            var originalList = new List<int>() { 1, 4, 5, 10 };
+            int newValue = 2;
+
+            var expected = new List<int>() { 1, 2, 4, 5, 10 };
+
+            var sut = new InsertNewValue();
+
+            // act
+            var actual = sut.AddNewValueToList(originalList, newValue);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
