8ad3f0978b04e4a7796bfee04df2a54427c930f1
brlom <brlom007@gmail.com>
2021-06-04 16:35:12 +0100

    started on binary solution for InsertNewValue

--- Katas/BE/BE2/InsertNewValue.cs
+++ Katas/BE/BE2/InsertNewValue.cs
@@ -8,7 +8,29 @@ namespace Katas.BE.BE2
     {
         public List<int> AddNewValueToList(List<int> intList, int intToAdd)
         {
+            // is list odd or even number
+            // if odd, we can find int in middle of list
+            // we then compare intToAdd with middle int
+            // if intToAdd is less than middle int, then keep everything to left of middle int and do same again until false
+            
+            
             return new List<int>();
+        }
+
+        public int BinarySearch(List<int> intList, int first, int last, int toAdd)
+        {
+            int middle = first + last / 2;
+            if (middle > toAdd)
+            {
+                return BinarySearch(intList, first, middle, toAdd);
+            }
+            else if (middle < toAdd)
+            {
+                return BinarySearch(intList, middle, last, toAdd);
+            }
         }
     }
 }
+
+// [1, 3, 4, 5, 6]
+// toAdd = 2;
